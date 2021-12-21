e8ec75b07ffd8b3387a51410f7496d11d19dbae3
Ties Stapper <ties.stapper@student.rocva.nl>
2021-12-21 20:12:00 +0100

    3.2

---
+++ 3-Arrays/3.2 - Array waarde wijzigen/index.php
@@ -0,0 +1,13 @@
+<?php
+$MyDognames = array("<li>Chop</li>", "<li>Bob</li>", "<li>Ben</li>");
+echo $MyDognames[0];
+echo $MyDognames[1];
+echo $MyDognames[2];
+
+$MyDognames = array("<li>Chop2</li>", "<li>Bob2</li>", "<li>Ben2</li>");
+echo $MyDognames[0];
+echo $MyDognames[1];
+echo $MyDognames[2];
+
+
+?> 
