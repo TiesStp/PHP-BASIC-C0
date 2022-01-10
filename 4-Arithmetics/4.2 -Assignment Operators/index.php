45b38b89eccf893348bca5e0985318c6023c4858
Ties Stapper <ties.stapper@student.rocva.nl>
2022-01-10 13:21:53 +0100

    4.2

---
+++ 4-Arithmetics/4.2 -Assignment Operators/index.php
@@ -0,0 +1,16 @@
+<?php
+$score = 0;
+echo "<li>$score</li>";
+
+$score += 12;
+echo "<li>$score</li>";
+
+$score -= 4;
+echo "<li>$score</li>";
+
+$score *= 2;
+echo "<li>$score</li>";
+
+$score /= 4;
+echo "<li>$score</li>";
+?>
