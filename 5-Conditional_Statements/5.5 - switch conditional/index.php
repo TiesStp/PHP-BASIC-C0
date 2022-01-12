c0c09f74826a89f4f7a5ee8ec648ac5697e395ed
Ties Stapper <ties.stapper@student.rocva.nl>
2022-01-12 12:58:22 +0100

    5.5

---
+++ 5-Conditional_Statements/5.5 - switch conditional/index.php
@@ -0,0 +1,19 @@
+<?php
+$score = 5;
+switch($score) {
+    case 39:
+    echo ("Wow wat een fantastische score!");
+    break;
+    case 5:
+    echo ("Wow bijna alle punten nog 1 te gaan!");
+    break;
+    case 55:
+    echo ("Goed bezig, bijna alle punten!");
+    break;
+    case 33:
+    echo ("Ga zo door!");
+    break;
+    default:
+    echo ("Nog even oefenen dan haal je vast alle punten!");
+}
+?>
